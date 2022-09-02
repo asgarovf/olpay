d920c204586c61391f4b2bdfba3c212f9729e355
asgarovf <farhad@getwhelp.com>
2022-09-02 23:31:23 +0100

    fix: backend

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ app.post("/api/subscribe", (req, res) => {
   const { email } = req.body;
 
   if (!email) {
+    console.log(email);
     return res.status(401).json({ msg: "error", code: 401 });
   }
 
